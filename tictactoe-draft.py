f4c78ea76f78d72faaffd9af8a7cc215cde5bd8c
Grzegorz Kalinowski <grzegorz.kalinowski@gmail.com>
2019-11-13 11:41:26 +0100

    Start of singleplayer

--- tictactoe-draft.py
+++ tictactoe-draft.py
@@ -39,9 +39,6 @@ def intro():
 
 
 def printmove(player_move , board, char):    #print board and player move
-    index = 0
-    char = char
-    player_move = player_move
     if player_move == 1:
         index = 1
     if player_move == 2:
@@ -90,13 +87,111 @@ def game_loop():
         #declare variables
     board = "|¹|²|³|\n|⁴|⁵|⁶|\n|⁷|⁸|⁹|"
     char = ""
-    playeronethinking = True
+    # playeronethinking = True
     possible_moves = [1,2,3,4,5,6,7,8,9]
     player_one_wins = 0
     player_two_wins = 0   
     player_one_moves = []
     player_two_moves = [] 
     player_one_name = input("Write first player name:")
+    player_two_name = input("Write second player name:")
+    wining_moves = [[1 , 2 , 3] , [4 , 5 , 6] , [7 , 8 , 9] , [1 , 5 , 9] ,
+                    [ 7, 5, 3] , [1 , 4 , 7] , [2 , 5 , 8] , [3 , 6 , 9]]
+    current_player=1
+    endgame = False
+    show_play_again=0
+
+    # main game loop
+
+    # while endgame != True:
+    #     if current_player==1:
+    #         char = "X"
+    #     elif current_player==2:
+    #         char = "O"
+    #     isCorrect = False
+    #     while isCorrect == False:
+    #         if current_player==1:
+    #             player_move_str = input(f"{Fore.GREEN}{player_one_name} - please make your move:{Style.RESET_ALL}")
+    #         elif current_player==2:
+    #             player_move_str = input(f"{Fore.BLUE}{player_two_name} - please make your move:{Style.RESET_ALL}")
+    #         try:
+    #             player_move = int(player_move_str)
+    #             possible_moves.index(player_move)
+    #             possible_moves.remove(player_move)
+    #             if current_player==1:
+    #                 player_one_moves.append(player_move)
+    #             elif current_player==2:
+    #                 player_two_moves.append(player_move)
+    #             isCorrect = True
+    #         except ValueError:
+    #             print("This is not a valid move!")
+                
+    #     print("\n")
+    #     board= printmove(player_move , board , char)
+    #     print("\n")
+
+    #     if current_player==1:
+    #         current_player=2
+    #     elif current_player==2:
+    #         current_player=1
+
+    #     for x in range(0, 8):                                       #check for win condition
+    #         list_0=list(set(wining_moves[x]).intersection(set(player_one_moves)))
+    #         list_1=list(set(wining_moves[x]).intersection(set(player_two_moves)))
+    #         if len(list_0) == 3:
+    #             print(player_one_name, "Wins!")
+    #             player_one_wins += 1
+    #             show_play_again = True
+    #         elif len(list_1) == 3:
+    #             print(player_two_name, "Wins!")
+    #             player_two_wins += 1
+    #             show_play_again = True
+
+    #     if show_play_again == True: 
+    #         show_play_again = False
+            
+    #         play_again = input("Play again? (y/n)")
+    #         if play_again == "y":
+    #             print("Starting new game")
+    #             possible_moves = [1,2,3,4,5,6,7,8,9]
+    #             board = "|¹|²|³|\n|⁴|⁵|⁶|\n|⁷|⁸|⁹|"
+    #             player_one_moves = []
+    #             player_two_moves = []
+    #             continue
+    #         else:
+    #             endgame = True
+                    
+    #     if not possible_moves:                          #check for tie
+    #         print("It's a tie.")
+    #         play_again = input("Play again? (y/n)")
+    #         if play_again == "y":
+    #             print("Starting new game")
+    #             possible_moves = [1,2,3,4,5,6,7,8,9]
+    #             board = "|¹|²|³|\n|⁴|⁵|⁶|\n|⁷|⁸|⁹|"
+    #             player_one_moves = []
+    #             player_two_moves = []
+    #             continue
+    #         else:
+    #             endgame = True
+    # show_scores(player_one_wins, player_two_wins, player_one_name, player_two_name)
+
+def computer_move_calc(possible_moves, player_two_moves, wining_moves):
+    comp_move = ""
+    if len(possible_moves) == 9:
+        comp_move = "5"
+
+    return comp_move
+
+def single_game_loop():
+    board = "|¹|²|³|\n|⁴|⁵|⁶|\n|⁷|⁸|⁹|"
+    char = ""
+    # playeronethinking = True
+    possible_moves = [1,2,3,4,5,6,7,8,9]
+    player_one_wins = 0
+    player_two_wins = 0   
+    player_one_moves = []
+    player_two_moves = [] 
+    player_one_name = "Computer"
     player_two_name = input("Write second player name:")
     wining_moves = [[1 , 2 , 3] , [4 , 5 , 6] , [7 , 8 , 9] , [1 , 5 , 9] ,
                     [ 7, 5, 3] , [1 , 4 , 7] , [2 , 5 , 8] , [3 , 6 , 9]]
@@ -114,7 +209,8 @@ def game_loop():
         isCorrect = False
         while isCorrect == False:
             if current_player==1:
-                player_move_str = input(f"{Fore.GREEN}{player_one_name} - please make your move:{Style.RESET_ALL}")
+                computer_move_calc(possible_moves, player_two_moves, wining_moves)
+                player_move_str = computer_move_calc()
             elif current_player==2:
                 player_move_str = input(f"{Fore.BLUE}{player_two_name} - please make your move:{Style.RESET_ALL}")
             try:
@@ -138,52 +234,11 @@ def game_loop():
         elif current_player==2:
             current_player=1
 
-        # playeronethinking = False                                   #variables for switching players
-        # playertwothinking = True
-        for x in range(0, 8):                                       #check for win condition
-            list_0=list(set(wining_moves[x]).intersection(set(player_one_moves)))
-            list_1=list(set(wining_moves[x]).intersection(set(player_two_moves)))
-            if len(list_0) == 3:
-                print(player_one_name, "Wins!")
-                player_one_wins += 1
-                show_play_again = True
-            elif len(list_1) == 3:
-                print(player_two_name, "Wins!")
-                player_two_wins += 1
-                show_play_again = True
-
-        if show_play_again == True: 
-            show_play_again = False
-            
-            play_again = input("Play again? (y/n)")
-            if play_again == "y":
-                print("Starting new game")
-                possible_moves = [1,2,3,4,5,6,7,8,9]
-                board = "|¹|²|³|\n|⁴|⁵|⁶|\n|⁷|⁸|⁹|"
-                player_one_moves = []
-                player_two_moves = []
-                continue
-            else:
-                endgame = True
-                    
-        if not possible_moves:                          #check for tie
-            print("It's a tie.")
-            play_again = input("Play again? (y/n)")
-            if play_again == "y":
-                print("Starting new game")
-                possible_moves = [1,2,3,4,5,6,7,8,9]
-                board = "|¹|²|³|\n|⁴|⁵|⁶|\n|⁷|⁸|⁹|"
-                player_one_moves = []
-                player_two_moves = []
-                continue
-            else:
-                endgame = True
-    show_scores(player_one_wins, player_two_wins, player_one_name, player_two_name)
-
 
 def main():                                               #main
     intro()                                               #show intro and initial board
-    game_loop()
+    # game_loop()
+    single_game_loop()
 ###########################endgame stats and credits##################################
     
     
