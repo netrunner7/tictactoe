79d0147a6719e0b2c97ad530c254412e70088147
Grzegorz Kalinowski <grzegorz.kalinowski@gmail.com>
2019-11-14 11:32:37 +0100

    added click sound

--- tictactoe-draft-v3.py
+++ tictactoe-draft-v3.py
@@ -1,45 +1,40 @@
 # # tictactoe
+#import
 import time
 import os
 import random
-from playsound import playsound
 from colorama import Fore
 from colorama import Style
 
 
 def intro():
     ##################################### Intro #################################
-        os.system('clear')
-        print(f'''{Fore.YELLOW}
-    __________     ___.  ___.                  ________                 __    
-    \______   \__ _\_ |__\_ |__   ___________  \______ \  __ __   ____ |  | __
-     |       _/  |  \ __ \| __ \_/ __ \_  __ \  |    |  \|  |  \_/ ___\|  |/ /
-     |    |   \  |  / \_\ \ \_\ \  ___/|  | \/  |    `   \  |  /\  \___|    < 
-     |____|_  /____/|___  /___  /\___  >__|    /_______  /____/  \___  >__|_  |
-            \/          \/    \/     \/                \/            \/     \/
-        {Style.RESET_ALL}''')
-        time.sleep(2)                 #finalnie 2 sekundy
-        os.system('clear')
-        print("Presents:")
-        time.sleep(2)
-        os.system('clear')
-        print('''
-    __ __|_)      __ __|          __ __|          
-       |   |  __|    |  _` |  __|    |  _ \   _ \ 
-       |   | (       | (   | (       | (   |  __/ 
-      _|  _|\___|   _|\__,_|\___|   _|\___/ \___|   
-        ''')
-        time.sleep(2)
+    #     print(f'''{Fore.YELLOW}
+    # __________     ___.  ___.                  ________                 __    
+    # \______   \__ _\_ |__\_ |__   ___________  \______ \  __ __   ____ |  | __
+    #  |       _/  |  \ __ \| __ \_/ __ \_  __ \  |    |  \|  |  \_/ ___\|  |/ /
+    #  |    |   \  |  / \_\ \ \_\ \  ___/|  | \/  |    `   \  |  /\  \___|    < 
+    #  |____|_  /____/|___  /___  /\___  >__|    /_______  /____/  \___  >__|_  |
+    #         \/          \/    \/     \/                \/            \/     \/
+    #     {Style.RESET_ALL}''')
+    #     time.sleep(2)                 #finalnie 2 sekundy
+    #     os.system('clear')
+    #     print("Presents:")
+    #     time.sleep(2)
+    #     os.system('clear')
+    #     print('''
+    # __ __|_)      __ __|          __ __|          
+    #    |   |  __|    |  _` |  __|    |  _ \   _ \ 
+    #    |   | (       | (   | (       | (   |  __/ 
+    #   _|  _|\___|   _|\__,_|\___|   _|\___/ \___|   
+    #     ''')
+    #     time.sleep(2)
         print('''
         Possible moves: 1, 2, 3, 4, 5, 6, 7, 8, 9 ; try to fill a row!
         |¹|²|³|     =     1 | 2 | 3
         |⁴|⁵|⁶|     =     4 | 5 | 6
         |⁷|⁸|⁹|     =     7 | 8 | 9
-
-        If you want to play with a computer, enter "Computer" as the first player
-
         ''')
-
 
 
 def printmove(player_move , board, char):    #print board and player move
@@ -63,11 +58,15 @@ def printmove(player_move , board, char):    #print board and player move
     if player_move == 9:
         index = 21
     board = board[0:index] + char + board[index + 1:]
+    os.system('clear')
+    time.sleep(1)
     print(board)
+    
+    return board
 
-    return board    
+### Endgame - Showscores ###
 
-def show_scores(player_one_wins, player_two_wins, name_first, name_second):
+def show_scores(player_one_wins, player_two_wins, name_first, name_second):   
     os.system('clear')                                          
     if player_one_wins == 1:                                            #exception for one game won
         print(name_first, "won:" , player_one_wins , "game.")
@@ -77,7 +76,49 @@ def show_scores(player_one_wins, player_two_wins, name_first, name_second):
         print(name_second, "won:" , player_two_wins , "game.")
     else:
         print(name_second, "won:" , player_two_wins , "games.")
-    print("\nThank you for playing! #skynet01 @ discord \n")
+    print("\nThank you for playing! #skynet01 #rubberduck @ discord \n")
+
+
+### AI ###
+def computer_move_calc(possible_moves, player_one_moves, player_two_moves, wining_moves):
+    comp_move = ""
+    corner = [1, 3, 7, 9]
+    if len(possible_moves) == 9:
+        comp_move = "5"
+    elif len(possible_moves) == 8:
+        comp_move = 5
+        if comp_move not in possible_moves:
+            comp_move = random.choice(corner)
+    elif len(possible_moves) == 7:
+        comp_move = random.choice(corner)
+        if comp_move not in possible_moves:
+            corner.remove(comp_move)
+            comp_move = random.choice(corner)
+    elif len(possible_moves) == 6 or 5 or 4 or 3 or 2:
+        for x in range(0, 8):
+            list_1=list(set(wining_moves[x]).intersection(set(player_two_moves)))
+            if len(list_1)== 2: 
+                ai_third_move2 = (set(wining_moves[x])) - (set(list_1))
+                ai_third_move_lst2 = list(ai_third_move2)
+                if ai_third_move_lst2[0] in possible_moves:
+                    comp_move = (ai_third_move_lst2[0])
+                    break
+            else:
+                comp_move = random.choice(possible_moves)
+        for x in range(0, 8): 
+            list_0=list(set(wining_moves[x]).intersection(set(player_one_moves)))
+            
+            if len(list_0)== 2:
+                ai_third_move = (set(wining_moves[x])) - (set(list_0))
+                ai_third_move_lst = list(ai_third_move)
+                if ai_third_move_lst[0] in possible_moves:
+                    comp_move = (ai_third_move_lst[0])
+        
+
+    elif len(possible_moves) == 1:
+        comp_move = possible_moves[0]
+        
+    return comp_move
 
 def game_loop():
 
@@ -90,14 +131,13 @@ def game_loop():
     player_two_wins = 0   
     player_one_moves = []
     player_two_moves = [] 
-    player_one_name = input("Write first player name: ").title()
-    player_two_name = input("Write second player name: ").title()
+    player_one_name = input("Write first player name:")
+    player_two_name = input("Write second player name:")
     wining_moves = [[1 , 2 , 3] , [4 , 5 , 6] , [7 , 8 , 9] , [1 , 5 , 9] ,
                     [ 7, 5, 3] , [1 , 4 , 7] , [2 , 5 , 8] , [3 , 6 , 9]]
     current_player=1
     endgame = False
     show_play_again=0
-    os.system('clear')
 
     # main game loop
 
@@ -107,14 +147,9 @@ def game_loop():
         elif current_player==2:
             char = "O"
         isCorrect = False
-        if len(possible_moves) == 9:
-            print("\n")
-            print(board)
-            print("\n")
         while isCorrect == False:
             if current_player==1 and player_one_name =="Computer":
                 player_move_str = computer_move_calc(possible_moves, player_one_moves, player_two_moves, wining_moves)
-                time.sleep(1)
             elif current_player==1:
                 player_move_str = input(f"{Fore.GREEN}{player_one_name} - please make your move:{Style.RESET_ALL}")
             elif current_player==2:
@@ -131,12 +166,9 @@ def game_loop():
             except ValueError:
                 print("This is not a valid move!")
                 
-        os.system('clear')
         print("\n")
         board= printmove(player_move , board , char)
-        playsound('/home/grzegorz/Documents/codecool/tictactoe/click.wav')
         print("\n")
-        
 
         if current_player==1:
             current_player=2
@@ -183,56 +215,11 @@ def game_loop():
                 endgame = True
     show_scores(player_one_wins, player_two_wins, player_one_name, player_two_name)
 
-def computer_move_calc(possible_moves, player_one_moves, player_two_moves, wining_moves):
-    comp_move = ""
-    corner = [1, 3, 7, 9]
-    if len(possible_moves) == 9:
-        comp_move = "5"
-    
-    elif len(possible_moves) == 8:
-        comp_move = 5
-        if comp_move not in possible_moves:
-            comp_move = random.choice(corner)
-    
-    elif len(possible_moves) == 7:
-        comp_move = random.choice(corner)
-        if comp_move not in possible_moves:
-            corner.remove(comp_move)
-            comp_move = random.choice(corner)
-    
-    elif len(possible_moves) == 6 or 5 or 4 or 3 or 2:
-        for x in range(0, 8):
-            list_1=list(set(wining_moves[x]).intersection(set(player_two_moves)))
-            if len(list_1)== 2: 
-                ai_third_move2 = (set(wining_moves[x])) - (set(list_1))
-                ai_third_move_lst2 = list(ai_third_move2)
-                if ai_third_move_lst2[0] in possible_moves:
-                    comp_move = (ai_third_move_lst2[0])
-                    break
-            else:
-                comp_move = random.choice(possible_moves)
-        for x in range(0, 8): 
-            list_0=list(set(wining_moves[x]).intersection(set(player_one_moves)))
-            
-            if len(list_0)== 2:
-                ai_third_move = (set(wining_moves[x])) - (set(list_0))
-                ai_third_move_lst = list(ai_third_move)
-                if ai_third_move_lst[0] in possible_moves:
-                    comp_move = (ai_third_move_lst[0])
-
-    elif len(possible_moves) == 1:
-        comp_move = possible_moves[0]
-
-        
-    
-    
-    return comp_move
 
 
 def main():                                               #main
     intro()                                               #show intro and initial board
-    game_loop()
-###########################endgame stats and credits##################################
-    
+    game_loop()                                           #full game loop and showscores at the end
+
     
 main()
